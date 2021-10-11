70af04ed89793b1369dd59d740ce5224e1f5d2b7
Guedes <diogofoguedes@hotmail.com>
2021-10-11 22:07:22 +0100

    Insert operations

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,17 +13,16 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Department obj = new Department(1,"Books");
+		Department department = new Department(1,"Books");
 	
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 
-		
-		List<Seller> list = sellerDao.findAll();
-		
-		for(Seller sellerboy : list) {
-			System.out.println(sellerboy);
-		}
+		Seller newSeller = new Seller(null,"Greg","greg@mail",new Date(),4000.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println(newSeller.getId());
 		
 	}
 
 }
+
